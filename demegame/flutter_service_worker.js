1f5f97528c95b0b5a4070a050c454cd69c095b4e
Nam Nguyen <nvpnam@gmail.com>
2023-04-03 22:15:33 -0700

    Small UI improvements

--- demegame/flutter_service_worker.js
+++ demegame/flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "702c4d01021678610946bbb03ac61333",
-"/": "702c4d01021678610946bbb03ac61333",
-"main.dart.js": "77a218cb9a87d582ee0fb5f02aeab197",
+"index.html": "04aa48d71283387c22e8be611318cb3a",
+"/": "04aa48d71283387c22e8be611318cb3a",
+"main.dart.js": "1b96f0b12c7bc6fe8705175b3782db56",
 "manifest.json": "e265752b36c4e3c37dbc8987e142f49c",
 "version.json": "4c34f3acd5852c99034c152ea4e85fea"
 };
